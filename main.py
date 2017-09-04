dfa35b7dbf2b9d082dc7f32ff358178c608df2fe
ByteShaker <friedrich.schweizer@bmw.de>
2017-09-04 15:51:32 +0200

    Final Run

--- main.py
+++ main.py
@@ -161,8 +161,8 @@ def run():
     runs_dir = './runs'
     tests.test_for_kitti_dataset(data_dir)
 
-    epochs = 12
-    batch_size = 1
+    epochs = 20
+    batch_size = 2
 
     # Download pretrained vgg model
     helper.maybe_download_pretrained_vgg(data_dir)
